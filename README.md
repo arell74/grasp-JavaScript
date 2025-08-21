fb758407dec55062fa3ef0734876bafdd6d403e4
arell74 <farelfauzan36@gmail.com>
2025-08-21 10:59:57 +0700

    Add: function declaration vs expression excercise

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # grasp-JavaScript
 A collection of my JavaScript practice codes, from basic syntax to fundamental concepts.
+![JavaScript](https://img.shields.io/badge/language-JavaScript-yellow)
---
+++ function-declaration-v-expression/script.js
@@ -0,0 +1,11 @@
+//function declaration
+function tampilPesan(nama) {
+    alert('halo' + nama);
+}
+
+//function expression
+var tampilPesan = function(nama){
+                    console.log('halo ' + nama);
+                }
+
+tampilPesan('farel');
