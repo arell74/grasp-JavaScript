76188640aa67d6d568908325f59649f06614b58a
arell74 <farelfauzan36@gmail.com>
2025-09-01 20:43:02 +0700

    Practice: DOM Manipulation part 1: classList

---
+++ DOM/dom-manipulation/script.js
@@ -0,0 +1,17 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<i>Farel Fauzan</i>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'Hello World';
+
+// const juduls = document.querySelector('#judul');
+// juduls.style.color = 'lightblue';
+// juduls.style.backgroundColor = 'navy';
+
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name', 'farel');
+
+// const a = document.querySelector('section#a a');
+
+const p2 = document.querySelector('.p2');
+
