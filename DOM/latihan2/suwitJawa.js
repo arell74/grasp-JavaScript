39043ac4d6356da061440e73db26edd0fe1253df
arell74 <farelfauzan36@gmail.com>
2025-09-05 21:16:22 +0700

    Exercise: latihan DOM 2. Game suit jawa part 2

--- DOM/latihan2/suwitJawa.js
+++ DOM/latihan2/suwitJawa.js
@@ -13,16 +13,77 @@ function getHasil(comp, player) {
     if( player == 'orang') return (comp == 'semut') ? 'MENANG' : 'KALAH';
 }
 
-const pGajah = document.querySelector('.gajah');
-pGajah.addEventListener('click', function() {
-    const pilihanComp = getPilihanComp();
-    const pilihanPlayer = pGajah.className;
-    const hasil = getHasil(pilihanComp, pilihanPlayer);
-
+function putar() {
     const imgComputer = document.querySelector('.img-komputer');
-    imgComputer.setAttribute('src', 'img/' + pilihanComp + '.png');
+    const gambar = ['gajah', 'semut', 'orang'];
+    let i = 0;
+    const waktuMulai = new Date().getTime();
 
-    const info = document.querySelector('.info');
-    info.innerHTML = hasil;
-})
+    setInterval(function(){
+        if( new Date().getTime() - waktuMulai > 1000 ) {
+            clearInterval;
+            return;
+        }
+        imgComputer.setAttribute('src', 'img/' + gambar[i++] + '.png');
+        if( i == gambar.length) i = 0;
+    }, 100)
+}
 
+
+const pilihan = document.querySelectorAll('li img');
+pilihan.forEach(function(pil) {
+    pil.addEventListener('click', function() {
+        const pilihanComp = getPilihanComp();
+        const pilihanPlayer = pil.className;
+        const hasil = getHasil(pilihanComp, pilihanPlayer);
+
+        putar();
+
+        setTimeout(function() {
+            const imgComputer = document.querySelector('.img-komputer');
+            imgComputer.setAttribute('src', 'img/' + pilihanComp + '.png');
+
+            const info = document.querySelector('.info');
+            info.innerHTML = hasil;
+        }, 1000);
+    });
+});
+
+// const pGajah = document.querySelector('.gajah');
+// pGajah.addEventListener('click', function() {
+//     const pilihanComp = getPilihanComp();
+//     const pilihanPlayer = pGajah.className;
+//     const hasil = getHasil(pilihanComp, pilihanPlayer);
+
+//     const imgComputer = document.querySelector('.img-komputer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComp + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// const pOrang = document.querySelector('.orang');
+// pOrang.addEventListener('click', function() {
+//     const pilihanComp = getPilihanComp();
+//     const pilihanPlayer = pOrang.className;
+//     const hasil = getHasil(pilihanComp, pilihanPlayer);
+
+//     const imgComputer = document.querySelector('.img-komputer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComp + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// const pSemut = document.querySelector('.semut');
+// pSemut.addEventListener('click', function() {
+//     const pilihanComp = getPilihanComp();
+//     const pilihanPlayer = pSemut.className;
+//     const hasil = getHasil(pilihanComp, pilihanPlayer);
+
+//     const imgComputer = document.querySelector('.img-komputer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComp + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
