7b1a696e29dcf6436374085f745dace2d9c9a635
arell74 <farelfauzan36@gmail.com>
2025-09-07 22:10:29 +0700

    Practice: DOM Event Bubbling

--- DOM/dom-universal/script.js
+++ DOM/dom-universal/script.js
@@ -5,7 +5,7 @@
 //     card.style.display = 'none';
 // });
 
-const close = document.querySelectorAll('.close');
+// const close = document.querySelectorAll('.close');
 
 // for( let i = 0; i < close.length; i++) {
 //     close[i].addEventListener('click', function(e) {
@@ -14,15 +14,31 @@ const close = document.querySelectorAll('.close');
 //     })
 // }
 
-close.forEach(function(el) {
-    el.addEventListener('click', function(e) {
+// close.forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//         e.target.parentElement.style.display = 'none';
+//         e.preventDefault();
+//         e.stopPropagation();
+//     });
+// });
+
+
+// const nama = document.querySelector('.nama');
+// // console.log(nama.nextElementSibling);
+// // console.log(nama.previousElement);
+// console.log(nama.nextElementSibling.nextElementSibling);
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function(card) {
+//     card.addEventListener('click', function(e) {
+//         alert('ok');
+//     })
+// })
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function(e) {
+    if(e.target.className == 'close') {
         e.target.parentElement.style.display = 'none';
         e.preventDefault();
-    });
-});
-
-
-const nama = document.querySelector('.nama');
-// console.log(nama.nextElementSibling);
-// console.log(nama.previousElement);
-console.log(nama.nextElementSibling.nextElementSibling);
+    }
+})
