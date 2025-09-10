25439e417ac9f61b1ca7376024682c3067d44497
arell74 <farelfauzan36@gmail.com>
2025-09-10 21:34:49 +0700

    AdvancedJS: Object.create()

--- ADVANCED-JS/object(revisited)/script.js
+++ ADVANCED-JS/object(revisited)/script.js
@@ -1,54 +1,72 @@
 // Membuat object di javascript
 
 // 1. Object Literal
-let mhs = {
-    nama : 'Farel',
-    energy : 10,
-    makan : function(porsi) {
-        this.energy = this.energy + porsi;
-        console.log(`Selamat makan ${this.nama}, wareg nyah`);
-    }
-}
+// PROBLEM: tidak efektif untuk object yang banyak
+// let mhs = {
+//     nama : 'Farel',
+//     energy : 10,
+//     makan : function(porsi) {
+//         this.energy = this.energy + porsi;
+//         console.log(`Selamat makan ${this.nama}, wareg nyah`);
+//     }
+// }
+
+// let mhs2 = {
+//     nama : 'Raiden',
+//     energy : 10,
+//     makan : function(porsi) {
+//         this.energy = this.energy + porsi;
+//         console.log(`Selamat makan ${this.nama}, wareg nyah`);
+//     }
+// }
 
 
 // 2. function declaration
 
-// function Mahasiswa(nama, energy) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energy = energy;
+const methodMahasiswa = {
+    makan : function(porsi) {
+        this.energy += porsi;
+        console.log(`Selamat makan ${this.nama}, Wareg nyah`);
+    },
 
-//     mahasiswa.makan = function(porsi) {
+    main : function(jam) {
+        this.energy -= jam;
+        console.log(`Halo ${this.nama}, Selamat buang waktu`);
+    },
+
+    tidur : function(jam) {
+        this.energy += jam * 2;
+        console.log(`Halo ${this.nama}, Oyasumi~`);
+    }
+}
+
+function Mahasiswa(nama, energy) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energy = energy;
+
+    return mahasiswa;
+} 
+
+let farel = Mahasiswa('farel', 100);
+let raiden = Mahasiswa('raiden', 100);
+
+
+// 3. constructor function
+// function Mahasiswa(nama, energy) {
+//     this.nama = nama;
+//     this.energy = energy;
+
+//     this.makan = function(porsi) {
 //         this.energy += porsi;
 //         console.log(`Selamat makan ${this.nama}, Wareg nyah`);
 //     }
 
-//     mahasiswa.main = function(jam) {
+//     this.main = function(jam) {
 //         this.energy -= jam;
 //         console.log(`Halo ${this.nama}, Selamat buang waktu`);
 //     }
 
-//     return mahasiswa;
 // } 
 
-// let farel = Mahasiswa('farel', 100);
-
-
-// 3. constructor function
-function Mahasiswa(nama, energy) {
-    this.nama = nama;
-    this.energy = energy;
-
-    this.makan = function(porsi) {
-        this.energy += porsi;
-        console.log(`Selamat makan ${this.nama}, Wareg nyah`);
-    }
-
-    this.main = function(jam) {
-        this.energy -= jam;
-        console.log(`Halo ${this.nama}, Selamat buang waktu`);
-    }
-
-} 
-
-let farel = new Mahasiswa('farel', 100);
+// let farel = new Mahasiswa('farel', 100);
